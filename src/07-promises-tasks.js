db3728f0b40a37c3c8b5b547866e98ebf5643ff4
naTpuk-ep <naTpuk.ep@gmail.com>
2020-12-14 23:55:06 +0300

    promises

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -28,8 +28,9 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  if (typeof isPositiveAnswer !== 'boolean') return Promise.reject(new Error('Wrong parameter is passed! Ask her again.'));
+  return isPositiveAnswer ? Promise.resolve('Hooray!!! She said "Yes"!') : Promise.resolve('Oh no, she said "No".');
 }
 
 
